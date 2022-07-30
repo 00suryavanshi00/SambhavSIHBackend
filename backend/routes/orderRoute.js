8dc213d0c0bb6f905b45ad56b0e6a72657e5a7f1
Sanskar Suryavanshi personal <ssuryavanshi0001@gmail.com>
2022-07-30 18:48:14 +0530

    removed redundant function

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const {newOrder, getSingleOrder, myOrders, getAllOrders, updateOrder, deleteOrder} = require("../controllers/orderController")
-const { isAuthenticatedUser,authorizeRoles, authorizeSHGAdmin } = require("../middleware/auth");
+const { isAuthenticatedUser,authorizeRoles } = require("../middleware/auth");
 
 router.route("/order/new").post(isAuthenticatedUser,newOrder)
 router.route("/order/:id").get(isAuthenticatedUser,getSingleOrder);
