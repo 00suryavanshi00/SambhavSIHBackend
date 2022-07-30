939ea538123fe980b1fefceba1bc7138b8ecc24f
Sanskar Suryavanshi personal <ssuryavanshi0001@gmail.com>
2022-07-30 10:26:51 +0530

    added shg-admin

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -29,3 +29,17 @@ exports.authorizeRoles = (...roles)=>{
         next();
     }
 }
+
+// exports.authorizeSHGAdmin = (...roles)=>{
+    
+//     return(req,res,next)=>{
+//         if(!roles.includes(req.user.role)){
+//             return next(
+//                 new ErrorHandler(
+//                     `Role : ${req.user.role} is not allowed to access this resource`
+//                     ,403))
+//         }
+
+//         next();
+//     }
+// }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -6,10 +6,10 @@ const { isAuthenticatedUser,authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
-router.route("/admin/product/new").post(isAuthenticatedUser,authorizeRoles("admin"), createProduct);
+router.route("/admin/product/new").post(isAuthenticatedUser,authorizeRoles("admin","shg-admin"), createProduct);
 router.route("/admin/product/:id")
-.put(isAuthenticatedUser,authorizeRoles("admin"), updateProduct)
-.delete(isAuthenticatedUser,authorizeRoles("admin"), deleteProduct);
+.put(isAuthenticatedUser,authorizeRoles("admin","shg-admin"), updateProduct)
+.delete(isAuthenticatedUser,authorizeRoles("admin","shg-admin"), deleteProduct);
 router.route("/product/:id").get(getProductDetails);
 router.route("/review").put(isAuthenticatedUser,createProductReview);
 router.route("/reviews").get(getProductReviews).delete(isAuthenticatedUser,deleteReview);
